8d3e11b758c352aeffff19d1c19cdbf92b989918
kirti <kirtiv4467@gmail.com>
2023-08-10 23:13:04 -0400

    working project

---
+++ ExtensionExample/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExtensionExample;
+
+Console.WriteLine("Please enter your name:");
+string input = Console.ReadLine();
+
+
+Console.WriteLine(input);
+
+bool output = input.StartsWithUpperCase();
+
+Console.WriteLine(input + " Starts with uppercase == " + output);
+
+
+
+
+
---
+++ ExtensionExample/stringExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionExample
+{
+    public  static class StringExtension
+    {
+        public static string reverseString(this string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                sb.Append(s[i]);
+            }
+
+
+            return sb.ToString();
+
+        }
+
+        public static bool StartsWithUpperCase(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return false;
+
+            char ch = s[0];
+            return char.IsUpper(ch);
+
+        }
+
+
+
+
+
+    }
+}
